e16ba78bd7f312b7aae69a8edc57a3c325a14d96
andre <andre@10.88.20.164>
2017-12-01 10:10:59 -0500

    daerd

--- src/trailPackage/Trail.java
+++ src/trailPackage/Trail.java
@@ -1,57 +1,64 @@
 package trailPackage;
+
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 
 public class Trail {
 	private int[] markers;
-	
+
 	public Trail() {
 		markers = new int[13];
 		importTrail();
 	}
+
 	public boolean isLevelTrailSegment(int start, int end) {
 		int max = 0;
 		int min = 0;
-		for(int i = start; i < end; i++) { // Finds the max and mins of the Trail Segment
-			if(markers[i] > max) {
+		for (int i = start; i < end; i++) { // Finds the max and mins of the Trail Segment
+			if (markers[i] > max) {
 				max = markers[i];
-			} else if(markers[i] < min) {
+			} else if (markers[i] < min) {
 				min = markers[i];
-			}	
+			}
 		}
-		if(max - min <= 10) { // Checks if difference between the max and min is less than 10
+		if (max - min <= 10) { // Checks if difference between the max and min is less than 10
 			return true; // If less than or equal to 10, return true
 		} else {
 			return false; // If more than 10, return false
 		}
 	}
-	
+
 	public boolean isDifficult() {
-		return false; //Here so that the program will compile, can delete this
+		return false; // Here so that the program will compile, can delete this
 	}
+
 	public int findMaxElevation() {
-		return 1;  //Here so that the program will compile, can delete this
+		return 1; // Here so that the program will compile, can delete this
 	}
+
 	public int findMaxElevation(int start, int end) {
-		return 1;  //Here so that the program will compile, can delete this
+		return 1; // Here so that the program will compile, can delete this
 	}
+
 	public int findMinElevation() {
-		return 1;  //Here so that the program will compile, can delete this
+		return 1; // Here so that the program will compile, can delete this
 	}
+
 	public int findMinElevation(int start, int end) {
-		return 1;  //Here so that the program will compile, can delete this
+		return 1; // Here so that the program will compile, can delete this
 	}
-	//Do not edit this method, no touchy
+
+	// Do not edit this method, no touchy
 	private void importTrail() {
 		try {
 			Scanner sn = new Scanner(new File("Trail.txt"));
-			for(int i = 0;i < markers.length; i++) {
+			for (int i = 0; i < markers.length; i++) {
 				markers[i] = Integer.parseInt(sn.nextLine());
 			}
-			
-		} catch(FileNotFoundException e) {
-			String s = "File not found! Make sure that Trail.txt " ;
+
+		} catch (FileNotFoundException e) {
+			String s = "File not found! Make sure that Trail.txt ";
 			s = s + "is in the same folder as the class.";
 			System.out.println(s);
 		}
